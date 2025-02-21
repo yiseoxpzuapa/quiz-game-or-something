43a50a2dff9d3ef3eb38866b127c06ff2c6042a4
yiseoxpzuapa <unharmedc724@gmail.com>
2025-02-21 08:45:20 -0600

    minor bug fix (idiot.py not working)

--- idiot.py
+++ idiot.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 messagebox.showinfo(" ","End it all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it  all End it ")
 root.quit()
 
-root = tk.Tk()
+# I DONT CARE IF THIS DOESNT WORK I DIIIFFFFFFFB
+root = tk
 root.mainloop()
 root.showfisc()
-
